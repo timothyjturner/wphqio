1096d847d2b74d3fa7d27d657e6590fb18230e24
Zaheer <u2utech1@gmail.com>
2024-11-11 12:49:01 +0500

    Setting up color plates and fixed gulp issues

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,6 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
-import sassCompiler from 'sass';  // Import the sass compiler
+import sassCompiler from 'sass';
 import autoprefixer from 'gulp-autoprefixer';
 import cleanCSS from 'gulp-clean-css';
 import rename from 'gulp-rename';
@@ -8,18 +8,23 @@ import rename from 'gulp-rename';
 // Set gulp-sass to use the installed Sass compiler
 const sassCompile = sass(sassCompiler);
 
-// Task to compile SCSS, autoprefix, and minify CSS
+// Task to compile, autoprefix, and minify main.scss
 gulp.task('styles', function() {
-    return gulp.src('src/scss/**/*.scss')  // Source SCSS files
+    return gulp.src('assets/scss/main.scss')  // Source main.scss file
         .pipe(sassCompile().on('error', sassCompile.logError))  // Compile SCSS to CSS
         .pipe(autoprefixer({  // Add vendor prefixes
             overrideBrowserslist: ['last 2 versions', '> 1%', 'ie 10'],
             cascade: false
         }))
         .pipe(cleanCSS())  // Minify CSS
-        .pipe(rename({ suffix: '.min' }))  // Rename to include '.min'
-        .pipe(gulp.dest('dist/css'));  // Output minified CSS
+        .pipe(rename('style.css'))  // Rename output file to style.css
+        .pipe(gulp.dest('dist/css'));  // Output to dist/css
 });
 
-// Define a default task that runs the styles task
-gulp.task('default', gulp.series('styles'));
+// Task to watch for changes in main.scss
+gulp.task('watch', function() {
+    gulp.watch('assets/scss/main.scss', gulp.series('styles'));  // Watch main.scss for changes
+});
+
+// Define the default task to run the styles task and start watching
+gulp.task('default', gulp.series('styles', 'watch'));
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPHQ</title>
+
+    <?php wp_head(); ?>
+</head>
+
+<header class="global-header">
+    <div class="container">
+        <div class="row">
+            <div class="header-logo col-md-3">
+                <img src="/wp-content/uploads/2024/11/wphq-logo.png">
+            </div>
+
+            <div class="header-nav col-md-9">
+                <div class="nav-wrapper">
+                    <?php 	
+                        wp_nav_menu( array( 'theme_location'=>'main-menu', 'container_class'=>'main-menu' ) ); 
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<body>
