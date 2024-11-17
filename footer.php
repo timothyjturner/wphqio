93ca13f72a772f4fadb614d788f0005eb7b9142e
Zaheer <u2utech1@gmail.com>
2024-11-17 21:31:52 +0500

    Footer changes

--- footer.php
+++ footer.php
@@ -2,10 +2,12 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <img src="/wp-content/uploads/2024/11/wphq-logo.png">
+                <img class="footer-logo" src="/wp-content/uploads/2024/11/wphq-logo.png">
             </div>
 
             <div class="col-md-4">
+                <h3>Quick Links</h3>
+
                 <?php 	
                     wp_nav_menu( array( 'theme_location'=>'footer-menu', 'container_class'=>'footer-menu' ) ); 
                 ?>
@@ -16,9 +18,10 @@
                 <a href="mailto:admin@wphq.io">Contact Us: admin@wphq.io</a>
             </div>
         </div>
-        <div class="copyright">
-            Copyright © 2024 WPHQ.
-        </div>
+    </div>
+
+    <div class="copyright text-center">
+        Copyright © 2024 WPHQ.
     </div>
 </footer>
 
