14fa87ffa9fa9ad6177d28369110e9ca31db2ecf
Zaheer <u2utech1@gmail.com>
2024-11-17 22:14:01 +0500

    Content updates and tweaks on front page

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <div class="container">
         <div class="row align-center">
             <div class="col-md-6 content">
-                <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+                <h1>Elevate Your WordPress Website with Proven Marketing Expertise</h1>
 
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>Your website deserves more than generic solutions. At WPHQ, we specialize in WordPress hosting, maintenance, and SEO services designed to drive results. Let us handle the technical side, so you can focus on growing your business.</p>
 
                 <div class="row">
                     <a class="white-btn" href="#">Learn More</a>
