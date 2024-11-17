dbef5b684f876e165ff7a7c14bf322340267edaa
Zaheer <u2utech1@gmail.com>
2024-11-17 11:26:56 +0500

    Banner fixes and Solutions Section

--- index.php
+++ index.php
@@ -9,12 +9,54 @@
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
                 <div class="row">
-                    <a href="#">Learn More</a>
+                    <a class="white-btn" href="#">Learn More</a>
                     <a class="primary-btn" href="#">Get Started</a>
                 </div>
             </div>
             <div class="col-md-6 img">
                 <img class="w-100" src="/wp-content/uploads/2024/11/home-hero-min-scaled.webp">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="solutions-sect">
+    <div class="container">
+        <h2>Solutions</h2>
+
+        <div class="row">
+            <div class="col-md-4 card">
+                <div class="inner">
+                    <img src="/wp-content/uploads/2024/11/server.png">
+
+                    <h3>Reliable Hosting & Hassle-Free Maintenance</h3>
+
+                    <p>Keep your WordPress site secure, fast, and up-to-date with our managed hosting and maintenance plans.</p>
+
+                    <a class="link" href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-md-4 card">
+                <div class="inner">
+                    <img src="/wp-content/uploads/2024/11/performance.png">
+
+                    <h3>SEO Services That Deliver Results</h3>
+
+                    <p>Take the guesswork out of search engine optimization. Our tailored strategies improve rankings, boost traffic, and drive leads.</p>
+
+                    <a class="link" href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-md-4 card">
+                <div class="inner">
+                    <img src="/wp-content/uploads/2024/11/coding.png">
+
+                    <h3>Bring Your Vision to Life</h3>
+
+                    <p>Need something unique? Our custom development services are designed to create powerful WordPress websites tailored to your exact needs.</p>
+
+                    <a class="link" href="#">Learn More</a>
+                </div>
             </div>
         </div>
     </div>
