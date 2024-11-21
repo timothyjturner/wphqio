6f5b4eac8faa3822d11b0d1837e45b69ee8fc465
Zaheer <u2utech1@gmail.com>
2024-11-21 16:26:43 +0500

    Dropdown styling and fixes

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,6 +2,7 @@ document.addEventListener('click', function (event) {
     if (event.target.classList.contains('primary-btn')) {
         event.preventDefault();
         let parentWrapper = event.target.closest('.btn-dropdown-wrapper');
+        let submenu = event.target.closest('.sub-menu');
 
         if (parentWrapper) {
             let dropdown = parentWrapper.querySelector('.dropdown');
@@ -10,9 +11,12 @@ document.addEventListener('click', function (event) {
                 event.target.classList.toggle('active');
                 dropdown.classList.toggle('active');
             }
+        }else if(submenu){
+            event.target.classList.toggle('active');
         }
+
         event.stopPropagation();
-    } else {
+    }else {
         document.querySelectorAll('.dropdown.active').forEach(function (dropdown) {
             dropdown.classList.remove('active');
         });
