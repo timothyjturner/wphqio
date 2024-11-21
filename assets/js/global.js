c4cff0521a30b667003179d1068ffc3f74c204c0
Zaheer <u2utech1@gmail.com>
2024-11-21 16:39:33 +0500

    Header Menu dropdown bug fix

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,7 +2,6 @@ document.addEventListener('click', function (event) {
     if (event.target.classList.contains('primary-btn')) {
         event.preventDefault();
         let parentWrapper = event.target.closest('.btn-dropdown-wrapper');
-        let submenu = event.target.closest('.sub-menu');
 
         if (parentWrapper) {
             let dropdown = parentWrapper.querySelector('.dropdown');
@@ -11,13 +10,36 @@ document.addEventListener('click', function (event) {
                 event.target.classList.toggle('active');
                 dropdown.classList.toggle('active');
             }
-        }else if(submenu){
-            event.target.classList.toggle('active');
         }
 
         event.stopPropagation();
     }else {
         document.querySelectorAll('.dropdown.active').forEach(function (dropdown) {
+            dropdown.classList.remove('active');
+        });
+        document.querySelectorAll('.primary-btn.active').forEach(function (button) {
+            button.classList.remove('active');
+        });
+    }
+});
+
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('primary-btn')) {
+        event.preventDefault();
+        let parentWrapper = event.target.closest('ul#menu-main-menu');
+
+        if (parentWrapper) {
+            let dropdown = parentWrapper.querySelector('.sub-menu');
+
+            if (dropdown) {
+                event.target.classList.toggle('active');
+                dropdown.classList.toggle('active');
+            }
+        }
+
+        event.stopPropagation();
+    }else {
+        document.querySelectorAll('.sub-menu.active').forEach(function (dropdown) {
             dropdown.classList.remove('active');
         });
         document.querySelectorAll('.primary-btn.active').forEach(function (button) {
