b3b04fff21812ad3337eb01609ac46385bb9fb0d
Naveed <cpsnaveed@gmail.com>
2024-12-14 04:11:32 +0500

    Adding WooCommerce funcitonality to the theme

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ add_action( 'wp_enqueue_scripts', 'wphqio_theme_setup' );
 function wphqio_theme_setup() {
   add_theme_support( 'title-tag' );
   add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'woocommerce' );
 
   wp_enqueue_style( 'wphqio-style', get_template_directory_uri() . '/dist/css/style.css', array(), rand(100, 1000));
 
