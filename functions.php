d26b1e22f65374609f116d2f000eedd29ce0dc01
Naveed <cpsnaveed@gmail.com>
2024-12-14 04:45:43 +0500

    Fixing Woocommerce issues

--- functions.php
+++ functions.php
@@ -1,13 +1,16 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'wphqio_theme_setup' );
 function wphqio_theme_setup() {
-  add_theme_support( 'title-tag' );
-  add_theme_support( 'post-thumbnails' );
-  add_theme_support( 'woocommerce' );
-
   wp_enqueue_style( 'wphqio-style', get_template_directory_uri() . '/dist/css/style.css', array(), rand(100, 1000));
 
   wp_enqueue_script( 'wphqio-js', get_template_directory_uri() . '/assets/js/global.js', array(), rand(100, 1000), true );
+}
+
+add_action( 'after_setup_theme', 'wphq_add_woocommerce_support' );
+function wphq_add_woocommerce_support() {
+    add_theme_support( 'title-tag' );
+  	add_theme_support( 'post-thumbnails' );
+  	add_theme_support( 'woocommerce' );
 }
 
 add_action( 'init', 'wpb_custom_new_menu' );
