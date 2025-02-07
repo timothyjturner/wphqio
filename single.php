27c057b326b121c789b92aeb52e8d0ed3ebf0380
u2utech1 <u2utech1@gmail.com>
2025-02-07 15:58:54 +0500

    global cta section add

--- single.php
+++ single.php
@@ -26,6 +26,52 @@ if ( have_posts() ) :
     </section>
     <!-- End of the Post -->
 
+
+
+
+    <section id="global-cta" class="cta" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay="200" data-aos-duration="800">
+                <div class="container">
+                    <div class="row align-center" style="background-color: <?=$global_cta['background_color']?>">
+                        <div class="col-md-8">
+                            <h2><?=$global_cta['title']?></h2>
+
+                            <?=$global_cta['description']?>
+
+                            <div class="row">
+                                <?php if($global_cta['buttons']): ?>
+                                    <?php foreach($global_cta['buttons'] as $key => $button): ?>
+                                        <?php if ($button['button']['title'] == 'Get Started'){
+                                            $global_button_links = get_field('global_button_links', 'option');
+                                            ?>
+                                                <div class="btn-dropdown-wrapper">
+                                                    <a class="primary-btn" href="#"><?=$buttons['button']['title']?></a>
+        
+                                                    <div class="dropdown">
+                                                        <ul class="links">
+                                                            <?php foreach($global_button_links as $global_links): ?>
+                                                                <li><a href="<?=$global_links['link']['url']?>"><?=$global_links['link']['title']?></a></li>
+                                                            <?php endforeach; ?>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            <?php
+                                        }else {
+                                            ?>
+                                                <a class="white-btn" href="<?=$button['button']['url']?>"><?=$button['button']['title']?></a>
+                                            <?php
+                                        } ?>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <img class="w-100" src="<?=$global_cta['image']['url']?>">
+                        </div>
+                    </div>
+                </div>
+            </section>
+
 <?php
     endwhile;
 else :
