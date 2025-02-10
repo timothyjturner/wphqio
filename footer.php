0ba472580097b92f88b3b71fbc99d0e73437fa2f
u2utech1 <u2utech1@gmail.com>
2025-02-10 22:15:51 +0500

    smooth scroll

--- footer.php
+++ footer.php
@@ -15,5 +15,15 @@
   AOS.init();
 </script>
 
+<script>
+    $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+</script>
+
 </body>
 </html>
