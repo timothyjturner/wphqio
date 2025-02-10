5a65127678b0f5b963e65d93d57404d7f6b90e1a
u2utech1 <u2utech1@gmail.com>
2025-02-10 22:43:50 +0500

    smooth scroll

--- footer.php
+++ footer.php
@@ -18,19 +18,33 @@
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 
 <script>
-// smooth scroll to anchor, with option of hash appearing in url. Thanks:
-// https://paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
-$(document).ready(function(){
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-        var target = this.hash;
-        var $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
-            // window.location.hash = target;
-        });
-    });
+$(function() {
+  setTimeout(function() {
+    if (location.hash) {
+      /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
+      window.scrollTo(0, 0);
+      target = location.hash.split('#');
+      smoothScrollTo($('#'+target[1]));
+    }
+  }, 1);
+  
+  // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      smoothScrollTo($(this.hash));
+      return false;
+    }
+  });
+  
+  function smoothScrollTo(target) {
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  }
 });
 </script>
 
