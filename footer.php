315e5bb92ceb73dd3cdfebb56e6f0095b253bc85
u2utech1 <u2utech1@gmail.com>
2025-02-10 22:28:41 +0500

    smooth scroll update

--- footer.php
+++ footer.php
@@ -14,6 +14,10 @@
 <script>
   AOS.init();
 </script>
+<script
+  src="https://code.jquery.com/jquery-3.7.1.slim.js"
+  integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc="
+  crossorigin="anonymous"></script>
 
 <script>
 $(function() {
