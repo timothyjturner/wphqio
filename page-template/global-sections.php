4cd6f8bab675f3d644567bcbc66d5810f2d45125
timothyjturner <timothyjturner@gmail.com>
2026-08-22 14:57:03 -0400

    pricing table output update

--- page-template/global-sections.php
+++ page-template/global-sections.php
@@ -292,7 +292,7 @@ if( have_rows('sections') ):
                             </label>
 
                             <span class="pricing-billing-label pricing-billing-label--annual is-active">Annual</span>
-                            <span class="pricing-billing-savings">Save 10% yearly</span>
+                            <span class="pricing-billing-savings">Save 10% yearly & gain other benefits</span>
                         </div>
                     <?php endif; ?>
 
