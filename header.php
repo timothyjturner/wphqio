70318aab68dc497bd13d57559440e797754d091f
timothyjturner <timothyjturner@gmail.com>
2026-08-26 09:29:30 -0400

    duplicate title fix

--- header.php
+++ header.php
@@ -3,16 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        <?php
-            bloginfo('name');
-            if (wp_title('', false)) {
-                echo '|';
-            } else {
-                echo bloginfo('description');
-            } wp_title('');
-        ?>
-    </title>
 
     <?php wp_head(); ?>
 
